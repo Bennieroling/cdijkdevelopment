650e34e710f264e57e585b6ffc8e4ef9f7b5d11a
FestinaLenteDev <92215679+Bennieroling@users.noreply.github.com>
2024-12-15 17:23:38 +0100

    update url in contact.tsx

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -16,7 +16,7 @@ const Contact = () => {
     setStatus("Sending...");
 
     try {
-      const response = await fetch("http://localhost:3001/api/send-message", {
+      const response = await fetch("https://c-dijk-dev.vercel.app/api/send-message", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
