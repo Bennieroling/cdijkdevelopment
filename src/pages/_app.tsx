69e9bf21602c2d0217c639f8bf4a2a1f33477232
Bennieroling <bjsvandijk@gmail.com>
2025-01-06 15:10:34 +0100

    update title to dev

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<>
-  <head> <title>C Dijk</title></head>
+  <head> <title>C Dijk Dev</title></head>
         <Hero />
         <Navbar />
     <Component {...pageProps} />
